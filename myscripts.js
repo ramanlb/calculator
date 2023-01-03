326151cfe2990b08a8bbcf131c439b73af2d3ba7
ramanlb <raman@lakebrains.in>
2023-01-03 15:36:05 +0530

    create a calculator

---
+++ myscripts.js
@@ -0,0 +1,33 @@
+// var result = document.getElementById('result').value
+
+function clr(){
+    document.getElementById('result').value = "";
+}
+
+function dis(val){
+    document.getElementById('result').value += val;
+}
+
+function myFunction(event){
+    if(event.key == '0' || event.key == '1' || event.key == '2' || event.key == '3' || event.key == '4' || event.key == '5' || event.key == '6' || event.key == '7' || event.key == '8' || event.key == '9' || event.key == '/'|| event.key == '+' || event.key == '-'
+    || event.key == '*' || event.key == '/'){
+        document.getElementById('result').value += event;
+    }
+}
+var cal = document.getElementById("calcu");
+cal.onkeyup = function (event) {
+    if (event.keyCode === 13) {
+        console.log("Enter");
+        let x = document.getElementById("result").value
+        console.log(x);
+        solve();
+    }
+}
+
+function solve(){
+    const x = document.getElementById('result').value
+    const y = eval(x)
+
+    document.getElementById('result').value = y
+
+}
